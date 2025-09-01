1b020644e99328508770f68c0b3a398871ae621b
yuj-2 <tree9030@naver.com>
2025-09-01 17:12:44 +0900

    .

--- studyPro/src/days01/Ex02.java
+++ studyPro/src/days01/Ex02.java
@@ -4,6 +4,6 @@ public class Ex02 {
 	public static void main(String[] args) {
 		System.out.println("복습"); 
 		System.out.println("복습"); 
-		System.out.println("복습"); 
+		System.out.println("복습");
 	}
 }
